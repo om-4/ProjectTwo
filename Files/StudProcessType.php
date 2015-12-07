1c11620fdfb4614916badf1700e64c795fcb4f18
om-4 <om4@umbc.edu>
2015-12-07 13:50:09 -0500

    Fix to making appointments to look better

--- Files/StudProcessType.php
+++ Files/StudProcessType.php
@@ -2,13 +2,12 @@
 session_start();
 
 //If we have a group appointment
-if ($_POST["type"] == "Group"){
-	$_SESSION["advisor"] = $_POST["type"];
-	header('Location: 08StudSelectTime.php');
+if ($_POST["advisor"] == "Group"){
+	include('08StudSelectTime.php');
 }
 
 //If we have an individual appointment
-elseif ($_POST["type"] == "Individual"){
+elseif ($_POST["advisor"] == "Individual"){
 	header('Location: 07StudSelectAdvisor.php');
 }
 ?>
